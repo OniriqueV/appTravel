8c83fcd98570ad2747917ae675696b050d49d0a4
minhphu711 <giangphu0502@gmail.com>
2025-11-18 19:16:04 +0700

    structure change
    (cherry picked from commit 316131ed6b966d84f388050463366c507e79d5e7)

---
+++ app/src/main/java/com/datn/apptravel/ui/discover/DiscoverViewModel.kt
@@ -0,0 +1,11 @@
+package com.datn.apptravel.ui.discover
+
+import com.datn.apptravel.ui.base.BaseViewModel
+
+class DiscoverViewModel : BaseViewModel() {
+
+    fun getGuides() {
+        // TODO: Implement with repository
+        setLoading(false)
+    }
+}
